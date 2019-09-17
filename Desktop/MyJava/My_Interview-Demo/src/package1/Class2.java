2ec8cfd439ce13750c198dbec9e3a3c566def67c
zonaed <zbhuiyan13@gmail.com>
2019-09-16 21:18:45 -0400

    dsadsa

---
+++ Desktop/MyJava/My_Interview-Demo/src/package1/Class2.java
@@ -0,0 +1,9 @@
+package package1;
+
+public class Class2 {
+
+	public  void main(String[] args) {
+		System.out.println("hello ratna");
+	}
+
+}
---
+++ Desktop/MyJava/My_Interview-Demo/src/package1/Class3.java
@@ -0,0 +1,10 @@
+package package1;
+
+public class Class3 {
+
+	public  void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
